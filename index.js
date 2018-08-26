e63836d29f339ee628af2a59d85ea81a53ca7fe8
Federico Gimenez <federico.gimenez@gmail.com>
2018-08-26 18:09:38 +0200

    some more tests and functionality

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-exports.getAnalysis = () => {
+exports.getAnalysis = (options) => {
+  if (!options || !options.bytecode) {
+    throw new Error('Please provide an options param with a bytecode member.')
+  }
+
+  return new Promise((resolve, reject) => {})
 }
--- test/index.js
+++ test/index.js
@@ -6,5 +6,19 @@ describe('main module', () => {
     it('is a function', () => {
       assert.ok(typeof main.getAnalysis === 'function')
     })
+
+    it('requires options', () => {
+      assert.throws(() => main.getAnalysis())
+    })
+
+    it('requires a bytecode option', () => {
+      assert.throws(() => main.getAnalysis({}))
+    })
+
+    it('returns a thenable', () => {
+      const result = main.getAnalysis({bytecode: 'my-bytecode'})
+
+      assert.ok(typeof result.then === 'function')
+    })
   })
 })
