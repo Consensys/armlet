914c5a2feee6a0ae4a87d1e4da3e7695bd43487c
Federico Gimenez <federico.gimenez@gmail.com>
2018-08-27 19:19:24 +0200

    first requester functionality tests and implementation

--- test/lib/requester.js
+++ test/lib/requester.js
@@ -1,24 +1,56 @@
-const requester = require('../../lib/requester')
+const nock = require('nock')
+const url = require('url')
 require('chai')
   .use(require('chai-as-promised'))
   .should()
 
+const requester = require('../../lib/requester')
+
 describe('requester', () => {
   describe('#do', () => {
-    describe('interface', () => {
-      it('is a function', () => {
-        requester.do.should.be.a('function')
-      })
-
-      it('returns a thenable', () => {
-        const result = requester.do({bytecode: 'my-bytecode', apiUrl: 'my-api-url'})
-
-        result.then.should.be.a('function')
-      })
-    })
-
     describe('functionality', () => {
+      const httpApiUrl = url.parse('http://localhost:3100')
+      const httpsApiUrl = url.parse('https://localhost:3100')
+      const validApiKey = 'valid-api-key'
+      const bytecode = 'bytecode'
+      const uuid = 'my-uuid'
+      const basePath = '/mythril/v1/analysis'
 
+      it('requests analysis for http API', async () => {
+        nock(`${httpApiUrl.href}`, {
+          reqheaders: {
+            authorization: `Bearer ${validApiKey}`
+          }
+        })
+          .post(basePath, {
+            type: 'bytecode',
+            contract: bytecode
+          })
+          .reply(200, {
+            result: 'Queued',
+            uuid: uuid
+          })
+
+        await requester.do({bytecode: bytecode}, httpApiUrl, validApiKey).should.eventually.equal(uuid)
+      })
+
+      it('requests analysis for https API', async () => {
+        nock(`${httpsApiUrl.href}`, {
+          reqheaders: {
+            authorization: `Bearer ${validApiKey}`
+          }
+        })
+          .post(basePath, {
+            type: 'bytecode',
+            contract: bytecode
+          })
+          .reply(200, {
+            result: 'Queued',
+            uuid: uuid
+          })
+
+        await requester.do({bytecode: bytecode}, httpsApiUrl, validApiKey).should.eventually.equal(uuid)
+      })
     })
   })
 })
