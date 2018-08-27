ceb3f26c273ecbbacce9433db9aedb54f7aa36d8
Federico Gimenez <federico.gimenez@gmail.com>
2018-08-27 18:29:31 +0200

    renamed input param

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const url = require('url')
 
 const defaultApiUrl = 'https://api.mythril.ai'
 
-exports.getAnalysis = (options, apiUrl = defaultApiUrl) => {
+exports.getAnalysis = (options, inputApiUrl = defaultApiUrl) => {
   return new Promise((resolve, reject) => {
     if (!options || !options.bytecode) {
       throw new TypeError('Please provide an options param with a bytecode member.')
     }
 
-    const parsedApiUrl = url.parse(apiUrl)
-    if (parsedApiUrl.hostname === null) {
-      throw new TypeError(`${apiUrl} is not a valid URL`)
+    const apiUrl = url.parse(inputApiUrl)
+    if (apiUrl.hostname === null) {
+      throw new TypeError(`${inputApiUrl} is not a valid URL`)
     }
   })
 }
