f64ee8b40e25b048e4efb9eccc831a4a877333d5
Federico Gimenez <federico.gimenez@gmail.com>
2018-08-27 19:41:59 +0200

    requester error handling

--- test/lib/requester.js
+++ test/lib/requester.js
@@ -51,6 +51,80 @@ describe('requester', () => {
 
         await requester.do({bytecode: bytecode}, httpsApiUrl, validApiKey).should.eventually.equal(uuid)
       })
+
+      it('rejects on api server connection failure', async () => {
+        const invalidApiHostname = url.parse('http://hostname')
+
+        await requester.do({bytecode: bytecode}, invalidApiHostname, validApiKey).should.be.rejectedWith(Error)
+      })
+
+      it('rejects on api server 500', async () => {
+        nock(`${httpApiUrl.href}`, {
+          reqheaders: {
+            authorization: `Bearer ${validApiKey}`
+          }
+        })
+          .post(basePath, {
+            type: 'bytecode',
+            contract: bytecode
+          })
+          .reply(500)
+
+        await requester.do({bytecode: bytecode}, httpApiUrl, validApiKey).should.be.rejectedWith(Error)
+      })
+
+      it('rejects on request limit errors', async () => {
+        const expectedErrorMsg = 'request limit exceeded'
+        nock(`${httpApiUrl.href}`, {
+          reqheaders: {
+            authorization: `Bearer ${validApiKey}`
+          }
+        })
+          .post(basePath, {
+            type: 'bytecode',
+            contract: bytecode
+          })
+          .reply(429, {
+            error: expectedErrorMsg
+          })
+
+        await requester.do({bytecode: bytecode}, httpApiUrl, validApiKey).should.be.rejectedWith(Error)
+      })
+
+      it('rejects on validation errors', async () => {
+        const expectedErrorMsg = 'validation failed'
+        nock(`${httpApiUrl.href}`, {
+          reqheaders: {
+            authorization: `Bearer ${validApiKey}`
+          }
+        })
+          .post(basePath, {
+            type: 'bytecode',
+            contract: bytecode
+          })
+          .reply(400, {
+            error: expectedErrorMsg
+          })
+
+        await requester.do({bytecode: bytecode}, httpApiUrl, validApiKey).should.be.rejectedWith(Error)
+      })
+
+      it('rejects on authentication errors', async () => {
+        const inValidApiKey = 'my-invalid-api--key-sigh'
+
+        nock(`${httpApiUrl.href}`, {
+          reqheaders: {
+            authorization: `Bearer ${inValidApiKey}`
+          }
+        })
+          .post(basePath, {
+            type: 'bytecode',
+            contract: bytecode
+          })
+          .reply(401, 'Unauthorized')
+
+        await requester.do({bytecode: bytecode}, httpApiUrl, inValidApiKey).should.be.rejectedWith(Error)
+      })
     })
   })
 })
