76ea0c207a13c83ef7747d489521d8cc478a8cdb
rocky <rocky.bernstein@mythril.ai>
2018-11-12 18:26:08 -0500

    Add typescript example and some fixes/tweaks...
    * Add typescript analysis example
    * fix syntax typo in api-version
    * Mythril Platform API -> Mythril Platform

--- example/typescript/analysis.ts
+++ example/typescript/analysis.ts
@@ -1,47 +1,60 @@
 #!/usr/bin/env node
 
-"use strict"
-const fs = require('fs')
+"use strict";
+import * as fs from 'fs';
 
 function usage() {
     console.log(`usage: ${process.argv[1]} [*mythril-api-json-path*] [*timeout-secs*]
 
-Run  Mythril Platform analyses on *mythril-api-json-path*
+Run Mythril Platform analyses on *mythril-api-json-path*
 
 Set environment variables EMAIL and MYTHRIL_API_KEY before using.
 `)
-    process.exit(1)
+    process.exit(1);
 }
 
-require('./helper')
+require('../helper');
 
 const argLen = process.argv.length
 if (argLen === 3 &&
     process.argv[2].match(/^[-]{0,2}h(?:elp)?$/)) {
-    usage()
+    usage();
 }
 
 let timeout = 20
 if (argLen >= 3 &&
     process.argv[argLen-1].match(/^\d+$/)) {
-    timeout = parseInt(process.argv[argLen-1])
+    timeout = parseInt(process.argv[argLen-1]);
 }
 
 
-const jsonPath = process.argv[2] || `${__dirname}/sample-json/PublicArray.json`
+const jsonPath = process.argv[2] || `${__dirname}/../sample-json/PublicArray.json`;
 
 /**********************************
   Example code starts here ...
 ***********************************/
 
+// What we use in a new armlet.Client()
+interface ArmletOptions {
+    apiKey: string;
+    userEmail: string;
+    platforms: Array<string>;
+};
+
+// What we use in a new armlet analyze call
+interface AnalyzeOptions {
+    data: any;  // Actually a JSON dictionary
+    timeout: number;
+};
+
 const options = {
     apiKey: process.env.MYTHRIL_API_KEY,
     userEmail: process.env.EMAIL,
     platforms: []  // client chargeback
-}
+};
 
-const armlet = require('../index') // if not installed
-// const armlet = require('armlet') // if installed
+const armlet = require('../../index'); // if not installed
+// import * as armlet from 'armlet' // if installed
 
 const client = new armlet.Client(options)
 
