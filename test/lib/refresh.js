cf0d522da797aec79349a2b14fab33fda9d1017d
Federico Gimenez <federico.gimenez@gmail.com>
2018-11-23 09:51:03 +0100

    fixed refresh

--- test/lib/refresh.js
+++ test/lib/refresh.js
@@ -1,4 +1,5 @@
 const nock = require('nock')
+const url = require('url')
 require('chai')
   .use(require('chai-as-promised'))
   .should()
@@ -7,7 +8,8 @@ const refresh = require('../../lib/refresh')
 
 describe('refresh', () => {
   describe('#do', () => {
-    const httpsApiUrl = 'https://localhost:3100'
+    const apiUrl = 'https://localhost:3100'
+    const parsedApiUrl = url.parse(apiUrl)
     const refreshPath = '/v1/auth/refresh'
     const expiredRefreshToken = 'expiredRefresh'
     const expiredAccessToken = 'expiredAccess'
@@ -15,11 +17,11 @@ describe('refresh', () => {
     const renewedJsonTokens = {refreshToken: 'renewedRefresh', accessToken: 'renewedAccess'}
 
     it('should return renewed refresh and access tokens', async () => {
-      nock(httpsApiUrl)
+      nock(apiUrl)
         .post(refreshPath, expiredJsonTokens)
         .reply(200, renewedJsonTokens)
 
-      await refresh.do(expiredRefreshToken, expiredAccessToken, httpsApiUrl).should.eventually.deep.equal(renewedJsonTokens)
+      await refresh.do(expiredRefreshToken, expiredAccessToken, parsedApiUrl).should.eventually.deep.equal(renewedJsonTokens)
     })
 
     it('should reject on api server connection failure', async () => {
@@ -29,35 +31,35 @@ describe('refresh', () => {
     })
 
     it('should reject on api server status code != 200', async () => {
-      nock(httpsApiUrl)
+      nock(apiUrl)
         .post(refreshPath, expiredJsonTokens)
         .reply(500)
 
-      await refresh.do(expiredRefreshToken, expiredAccessToken, httpsApiUrl).should.be.rejectedWith(Error, 'Invalid status code')
+      await refresh.do(expiredRefreshToken, expiredAccessToken, parsedApiUrl).should.be.rejectedWith(Error, 'Invalid status code')
     })
 
     it('should reject on non-JSON data', async () => {
-      nock(httpsApiUrl)
+      nock(apiUrl)
         .post(refreshPath, expiredJsonTokens)
         .reply(200, 'newjsonTextTokens')
 
-      await refresh.do(expiredRefreshToken, expiredAccessToken, httpsApiUrl).should.be.rejectedWith(Error, 'JSON parse error')
+      await refresh.do(expiredRefreshToken, expiredAccessToken, parsedApiUrl).should.be.rejectedWith(Error, 'JSON parse error')
     })
 
     it('should reject if refreshToken is not present in response', async () => {
-      nock(httpsApiUrl)
+      nock(apiUrl)
         .post(refreshPath, expiredJsonTokens)
         .reply(200, {accessToken: 'access'})
 
-      await refresh.do(expiredRefreshToken, expiredAccessToken, httpsApiUrl).should.be.rejectedWith(Error, 'Refresh Token missing')
+      await refresh.do(expiredRefreshToken, expiredAccessToken, parsedApiUrl).should.be.rejectedWith(Error, 'Refresh Token missing')
     })
 
     it('should reject if accessToken is not present', async () => {
-      nock(httpsApiUrl)
+      nock(apiUrl)
         .post(refreshPath, expiredJsonTokens)
         .reply(200, {refreshToken: 'refresh'})
 
-      await refresh.do(expiredRefreshToken, expiredAccessToken, httpsApiUrl).should.be.rejectedWith(Error, 'Access Token missing')
+      await refresh.do(expiredRefreshToken, expiredAccessToken, parsedApiUrl).should.be.rejectedWith(Error, 'Access Token missing')
     })
   })
 })
