04f79232d7f0ac1000ba19a81e3b8007310374f7
Federico Gimenez <federico.gimenez@gmail.com>
2018-11-23 09:49:03 +0100

    fix login

--- test/lib/login.js
+++ test/lib/login.js
@@ -1,4 +1,5 @@
 const nock = require('nock')
+const url = require('url')
 require('chai')
   .use(require('chai-as-promised'))
   .should()
@@ -7,7 +8,8 @@ const login = require('../../lib/login')
 
 describe('login', () => {
   describe('#do', () => {
-    const httpsApiUrl = 'https://localhost:3100'
+    const apiUrl = 'https://localhost:3100'
+    const parsedApiUrl = url.parse(apiUrl)
     const email = 'content'
     const password = 'password'
     const auth = { email, password }
@@ -15,49 +17,50 @@ describe('login', () => {
     const jsonTokens = { refreshToken: 'refresh', accessToken: 'access' }
 
     it('should return refresh and access tokens', async () => {
-      nock(httpsApiUrl)
+      nock(apiUrl)
         .post(loginPath, auth)
         .reply(200, jsonTokens)
 
-      await login.do(email, password, httpsApiUrl).should.eventually.deep.equal(jsonTokens)
+      await login.do(email, password, parsedApiUrl).should.eventually.deep.equal(jsonTokens)
     })
 
     it('should reject on api server connection failure', async () => {
       const invalidApiHostname = 'http://not-a-valid-hostname'
+      const parsedInvalidApiUrl = url.parse(invalidApiHostname)
 
-      await login.do(email, password, invalidApiHostname).should.be.rejectedWith(Error)
+      await login.do(email, password, parsedInvalidApiUrl).should.be.rejectedWith(Error)
     })
 
     it('should reject on api server status code != 200', async () => {
-      nock(httpsApiUrl)
+      nock(apiUrl)
         .post(loginPath, auth)
         .reply(500)
 
-      await login.do(email, password, httpsApiUrl).should.be.rejectedWith(Error, 'Invalid status code')
+      await login.do(email, password, parsedApiUrl).should.be.rejectedWith(Error, 'Invalid status code')
     })
 
     it('should reject on non-JSON data', async () => {
-      nock(httpsApiUrl)
+      nock(apiUrl)
         .post(loginPath, auth)
         .reply(200, 'jsonTextTokens')
 
-      await login.do(email, password, httpsApiUrl).should.be.rejectedWith(Error, 'JSON parse error')
+      await login.do(email, password, parsedApiUrl).should.be.rejectedWith(Error, 'JSON parse error')
     })
 
     it('should reject if refreshToken is not present', async () => {
-      nock(httpsApiUrl)
+      nock(apiUrl)
         .post(loginPath, auth)
         .reply(200, {accessToken: 'access'})
 
-      await login.do(email, password, httpsApiUrl).should.be.rejectedWith(Error, 'Refresh Token missing')
+      await login.do(email, password, parsedApiUrl).should.be.rejectedWith(Error, 'Refresh Token missing')
     })
 
     it('should reject if accessToken is not present', async () => {
-      nock(httpsApiUrl)
+      nock(apiUrl)
         .post(loginPath, auth)
         .reply(200, {refreshToken: 'refresh'})
 
-      await login.do(email, password, httpsApiUrl).should.be.rejectedWith(Error, 'Access Token missing')
+      await login.do(email, password, parsedApiUrl).should.be.rejectedWith(Error, 'Access Token missing')
     })
   })
 })
