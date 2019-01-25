819df8f4309d336695a14c9ce1c0e225ee2a989d
rocky <rocky@gnu.org>
2019-01-25 17:11:53 -0500

    more compilete API
    * add analyzeWithStatus() - runs deprecated analyze() and melds in analyzeStatus() in result
    * add analyzeStatus() - gets status by UUID
    * add analyzeIssues() - gets issue information from past runs
    * example/analysis-status use of above
    * lib/simpleRequester.js: better status on HTTP 401 results
    
    Start documenting calls better

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ const defaultApiUrl = process.env['MYTHX_API_URL'] || 'https://api.mythx.io'
 const defaultApiVersion = 'v1'
 
 class Client {
+  /**
+   *  Creates a client object. Internally this has login information, and from that
+   *  an access token. By saving state, we can use and refresh the access token
+   *  periodically.
+   *
+   *  @param {auth} object         - login or authentication information which contains
+   *                               (email | ethAddress) and a password or...
+   *                                apiKey
+   *  @param {inputApiUrl} string  - Optional. A URL of a MythX API server we want to contect
+   *                                 to.
+   *
+   */
   constructor (auth, inputApiUrl = defaultApiUrl) {
     if (!auth) {
       throw new TypeError('Please provide auth options.')
@@ -37,6 +49,19 @@ class Client {
     this.apiUrl = apiUrl
   }
 
+  /**
+    * Runs MythX analysis.
+    * Deprecated. See analyzeWithStatus() instead.
+    *
+    * @param {options} object - structure which must contain
+    *      {data} object       - information containing Smart Contract information to be analyzed
+    *      {timeout} number    - optional timeout value in milliseconds
+    *
+    * @returns an array-like object of issues, and a uuid attribute which can
+    *          be subsequently used to retrieve the information from our stored
+    *          database using analyzeIssues().
+    *
+    **/
   async analyze (options) {
     if (options === undefined || options.data === undefined || options.data.deployedBytecode === undefined) {
       throw new TypeError('Please provide a deployedBytecode option.')
@@ -75,9 +100,20 @@ class Client {
 
       result = await poller.do(uuid, this.accessToken, this.apiUrl, undefined, options.timeout)
     }
+    result.uuid = uuid
     return result
   }
 
+  /**
+    * Retrieves status records of past MythX analyses requests.
+    *
+    * @param {options} object - structure which must contain
+    *      {data} object       - information containing Smart Contract information to be analyzed
+    *      {timeout} number    - optional timeout value in milliseconds
+    *
+    * @returns an array-like object of issue status from our stored database.
+    *
+    **/
   async analyses (options) {
     if (options === undefined || options.dateFrom === undefined) {
       throw new TypeError('Please provide a dateFrom option.')
@@ -103,6 +139,49 @@ class Client {
       analyses = await simpleRequester.do({ url, accessToken: this.accessToken, json: true })
     }
     return analyses
+  }
+
+  /**
+    * Runs MythX analysis return issue information and metadata regarding the run
+    *
+    * @param {options} object - structure which must contain:
+    *      {data} object       - information containing Smart Contract information to be analyzed
+    *      {timeout} number    - optional timeout value in milliseconds
+    *
+    * @returns object which contains:
+    *      (issues}  object - an like object which of issues is grouped by (file) input container.
+    *      {status}  object - status information as returned in each object of analyses().
+    *
+    **/
+  async analyzeWithStatus (options) {
+    const issues = await this.analyze(options)
+    const uuid = issues.uuid
+    delete issues.uuid
+    const status = await this.analyzeStatus(uuid)
+    return {
+      issues,
+      status
+    }
+  }
+
+  async analyzeStatus (uuid, inputApiUrl = defaultApiUrl) {
+    let accessToken = this.accessToken
+    if (!accessToken) {
+      const tokens = await login.do(this.email, this.ethAddress, this.password, this.apiUrl)
+      accessToken = tokens.access
+    }
+    const url = `${inputApiUrl}/${defaultApiVersion}/analyses/${uuid}`
+    return simpleRequester.do({ url, accessToken: accessToken, json: true })
+  }
+
+  async analyzeIssues (uuid, inputApiUrl = defaultApiUrl) {
+    let accessToken = this.accessToken
+    if (!accessToken) {
+      const tokens = await login.do(this.email, this.ethAddress, this.password, this.apiUrl)
+      accessToken = tokens.access
+    }
+    const url = `${inputApiUrl}/${defaultApiVersion}/analyses/${uuid}/issues`
+    return simpleRequester.do({ url, accessToken: accessToken, json: true })
   }
 }
 
