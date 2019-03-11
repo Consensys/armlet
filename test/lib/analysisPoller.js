f988b5d74e1debb75298402e4588381530c74347
Daniyar Chambylov <dan.chambylov@gmail.com>
2019-03-11 14:24:36 +0700

    Fix tests for analysisPoller

--- test/lib/analysisPoller.js
+++ test/lib/analysisPoller.js
@@ -10,8 +10,7 @@ const util = require('../../lib/util')
 
 describe('analysisPoller', () => {
   describe('#do', () => {
-    const defaultApiUrl = new url.URL('https://api.mythx.io')
-    const httpApiUrl = new url.URL('http://localhost:3100')
+    const apiUrl = new url.URL('https://api.mythx.io')
     const validApiKey = 'valid-api-key'
     const uuid = 'my-uuid'
     const statusUrl = `/v1/analyses/${uuid}`
@@ -27,6 +26,12 @@ describe('analysisPoller', () => {
       }
     ]
 
+    const client = {
+      accessToken: validApiKey,
+      refreshToken: 'refreshToken',
+      apiUrl: apiUrl
+    }
+
     afterEach(() => {
       util.timer.restore()
     })
@@ -35,10 +40,9 @@ describe('analysisPoller', () => {
       sinon.stub(util, 'timer')
     })
 
-    it.skip('should poll issues with empty results', async () => {
+    it('should poll issues with empty results', async () => {
       const emptyResult = []
-
-      nock(defaultApiUrl.href, {
+      nock(apiUrl.href, {
         reqheaders: {
           authorization: `Bearer ${validApiKey}`
         }
@@ -48,7 +52,7 @@ describe('analysisPoller', () => {
         .reply(200, {
           status: 'In progress'
         })
-      nock(defaultApiUrl.href, {
+      nock(apiUrl.href, {
         reqheaders: {
           authorization: `Bearer ${validApiKey}`
         }
@@ -57,7 +61,7 @@ describe('analysisPoller', () => {
         .reply(200, {
           status: 'Finished'
         })
-      nock(defaultApiUrl.href, {
+      nock(apiUrl.href, {
         reqheaders: {
           authorization: `Bearer ${validApiKey}`
         }
@@ -65,11 +69,11 @@ describe('analysisPoller', () => {
         .get(issuesUrl)
         .reply(200, emptyResult)
 
-      await poller.do(uuid, validApiKey, defaultApiUrl, 10000, 5000).should.eventually.deep.equal(emptyResult)
+      await poller.do(uuid, client, 10000, 5000).should.eventually.deep.equal(emptyResult)
     })
 
-    it.skip('should poll issues with non-empty results', async () => {
-      nock(defaultApiUrl.href, {
+    it('should poll issues with non-empty results', async () => {
+      nock(apiUrl.href, {
         reqheaders: {
           authorization: `Bearer ${validApiKey}`
         }
@@ -79,7 +83,7 @@ describe('analysisPoller', () => {
         .reply(200, {
           status: 'In progress'
         })
-      nock(defaultApiUrl.href, {
+      nock(apiUrl.href, {
         reqheaders: {
           authorization: `Bearer ${validApiKey}`
         }
@@ -88,7 +92,7 @@ describe('analysisPoller', () => {
         .reply(200, {
           status: 'Finished'
         })
-      nock(defaultApiUrl.href, {
+      nock(apiUrl.href, {
         reqheaders: {
           authorization: `Bearer ${validApiKey}`
         }
@@ -96,11 +100,11 @@ describe('analysisPoller', () => {
         .get(issuesUrl)
         .reply(200, expectedIssues)
 
-      await poller.do(uuid, validApiKey, defaultApiUrl, 10000, 5000).should.eventually.deep.equal(expectedIssues)
+      await poller.do(uuid, client, 10000, 5000).should.eventually.deep.equal(expectedIssues)
     })
 
-    it.skip('should be able to query http API', async () => {
-      nock(httpApiUrl.href, {
+    it('should be able to query http API', async () => {
+      nock(apiUrl.href, {
         reqheaders: {
           authorization: `Bearer ${validApiKey}`
         }
@@ -110,7 +114,7 @@ describe('analysisPoller', () => {
         .reply(200, {
           status: 'In progress'
         })
-      nock(httpApiUrl.href, {
+      nock(apiUrl.href, {
         reqheaders: {
           authorization: `Bearer ${validApiKey}`
         }
@@ -119,7 +123,7 @@ describe('analysisPoller', () => {
         .reply(200, {
           status: 'Finished'
         })
-      nock(httpApiUrl.href, {
+      nock(apiUrl.href, {
         reqheaders: {
           authorization: `Bearer ${validApiKey}`
         }
@@ -127,13 +131,11 @@ describe('analysisPoller', () => {
         .get(issuesUrl)
         .reply(200, expectedIssues)
 
-      // FIXME: validApiKey shodl be an armlet client
-      // Also DRY code
-      await poller.do(uuid, validApiKey, 10000, 5000).should.eventually.deep.equal(expectedIssues)
+      await poller.do(uuid, client, 10000, 5000).should.eventually.deep.equal(expectedIssues)
     })
 
     it('should reject on server error', async () => {
-      nock(defaultApiUrl.href, {
+      nock(apiUrl.href, {
         reqheaders: {
           authorization: `Bearer ${validApiKey}`
         }
@@ -141,12 +143,11 @@ describe('analysisPoller', () => {
         .get(statusUrl)
         .reply(500)
 
-      // FIXME: validApiKey should be an armlet client
-      await poller.do(uuid, validApiKey, 10000, 5000).should.be.rejected
+      await poller.do(uuid, client, 10000, 5000).should.be.rejected
     })
 
     it('should reject on non-JSON data', async () => {
-      nock(defaultApiUrl.href, {
+      nock(apiUrl.href, {
         reqheaders: {
           authorization: `Bearer ${validApiKey}`
         }
@@ -156,7 +157,7 @@ describe('analysisPoller', () => {
         .reply(200, {
           status: 'In progress'
         })
-      nock(defaultApiUrl.href, {
+      nock(apiUrl.href, {
         reqheaders: {
           authorization: `Bearer ${validApiKey}`
         }
@@ -165,7 +166,7 @@ describe('analysisPoller', () => {
         .reply(200, {
           status: 'Finished'
         })
-      nock(defaultApiUrl.href, {
+      nock(apiUrl.href, {
         reqheaders: {
           authorization: `Bearer ${validApiKey}`
         }
@@ -173,13 +174,12 @@ describe('analysisPoller', () => {
         .get(issuesUrl)
         .reply(200, 'non-json-data')
 
-      // FIXME: validApiKey should be an armlet client
-      await poller.do(uuid, validApiKey, 10000, 5000).should.be.rejected
+      await poller.do(uuid, client, 10000, 5000).should.be.rejected
     })
 
     it('should reject after a timeout', async () => {
       const timeout = 15
-      nock(defaultApiUrl.href, {
+      nock(apiUrl.href, {
         reqheaders: {
           authorization: `Bearer ${validApiKey}`
         }
@@ -190,15 +190,13 @@ describe('analysisPoller', () => {
           status: 'In progress'
         })
 
-      // FIXME: validApiKey should be an armlet client
-      await poller.do(uuid, validApiKey, timeout, 5000).should.be
-        .rejected
+      await poller.do(uuid, client, timeout, 5000).should.be.rejected
     })
 
-    it.skip('should wait for initialDelay', async () => {
+    it('should wait for initialDelay', async () => {
       const emptyResult = []
 
-      nock(defaultApiUrl.href, {
+      nock(apiUrl.href, {
         reqheaders: {
           authorization: `Bearer ${validApiKey}`
         }
@@ -208,7 +206,7 @@ describe('analysisPoller', () => {
         .reply(200, {
           status: 'In progress'
         })
-      nock(defaultApiUrl.href, {
+      nock(apiUrl.href, {
         reqheaders: {
           authorization: `Bearer ${validApiKey}`
         }
@@ -217,7 +215,7 @@ describe('analysisPoller', () => {
         .reply(200, {
           status: 'Finished'
         })
-      nock(defaultApiUrl.href, {
+      nock(apiUrl.href, {
         reqheaders: {
           authorization: `Bearer ${validApiKey}`
         }
@@ -226,15 +224,15 @@ describe('analysisPoller', () => {
         .reply(200, emptyResult)
 
       const initialDelay = 5000
-      await poller.do(uuid, validApiKey, defaultApiUrl, 10000, 5000).should.eventually.deep.equal(emptyResult)
+      await poller.do(uuid, client, 10000, 5000).should.eventually.deep.equal(emptyResult)
       const delay = util.timer.getCall(0).args[0]
       delay.should.be.equal(initialDelay)
     })
 
-    it.skip('should wait for polling longer each time', async () => {
+    it('should wait for polling longer each time', async () => {
       const emptyResult = []
 
-      nock(defaultApiUrl.href, {
+      nock(apiUrl.href, {
         reqheaders: {
           authorization: `Bearer ${validApiKey}`
         }
@@ -244,7 +242,7 @@ describe('analysisPoller', () => {
         .reply(200, {
           status: 'In progress'
         })
-      nock(defaultApiUrl.href, {
+      nock(apiUrl.href, {
         reqheaders: {
           authorization: `Bearer ${validApiKey}`
         }
@@ -253,7 +251,7 @@ describe('analysisPoller', () => {
         .reply(200, {
           status: 'Finished'
         })
-      nock(defaultApiUrl.href, {
+      nock(apiUrl.href, {
         reqheaders: {
           authorization: `Bearer ${validApiKey}`
         }
@@ -262,7 +260,7 @@ describe('analysisPoller', () => {
         .reply(200, emptyResult)
 
       let lastDelay = 0
-      await poller.do(uuid, validApiKey, defaultApiUrl, 10000, 5000).should.eventually.deep.equal(emptyResult)
+      await poller.do(uuid, client, 10000, 5000).should.eventually.deep.equal(emptyResult)
       for (const i of [1, 2, 3]) {
         const nextDelay = util.timer.getCall(i).args[0]
         nextDelay.should.be.above(lastDelay)
@@ -271,7 +269,7 @@ describe('analysisPoller', () => {
     })
 
     it('should reject after maximum polling reached', async () => {
-      nock(defaultApiUrl.href, {
+      nock(apiUrl.href, {
         reqheaders: {
           authorization: `Bearer ${validApiKey}`
         }
